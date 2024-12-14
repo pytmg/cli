3d0d4cca2b7f0cf9e41a21fff5a2325b87f0a037
TMG <kristersand7@gmail.com>
2024-12-14 20:43:28 +0000

    Update example.py

--- example.py
+++ example.py
@@ -7,13 +7,13 @@ cli.addItem("Say \"Hello, World\"!")
 def sayHelloWorld():
     cli.print("Hello, World!")
 
-cli.addFunction(idx=0, func=sayHelloWorld, args=())
+cli.addFunction(0, sayHelloWorld, ())
 
 cli.addItem("/f red/Coloured /f green/Item/reset/!")
 
 def colouredResponse():
     cli.print("Wow!")
 
-cli.addFunction(idx=1, func=colouredResponse, args=())
+cli.addFunction(1, colouredResponse, ())
 
 cli.run()
