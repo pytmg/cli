39cfb5d62a1324aa3c88f5efea6df3ac1b751c6b
pytmg <190524216+pytmg@users.noreply.github.com>
2025-09-04 17:29:02 +0100

    Add EverythingIsRed theme

--- CustomThemeExample.py
+++ CustomThemeExample.py
@@ -16,3 +16,23 @@ class CustomThemes(Themes):
             self.theme_description = "A default theme with curved borders."
             self.theme_author = "tmg"
             self.border = Borders.SingleStroke_Curved()
+
+    class EIR(Themes.Default):
+        def __init__(self):
+            super().__init__()
+            self.theme_name = "/Rx/EVERYTHING IS /rx/RED/Rx/.//"
+            self.theme_description = "A fully red theme."
+            self.theme_author = "tmg"
+            self.border = Borders.Default() # not None
+
+            self.title = "/Rx/[title]//"
+            self.defaultOption = "/Rx/  [option]//"
+            self.disabledOption = "/rx/  [option]//"
+            self.selectedOption = "/Rx/> [option]//"
+            self.inp = " /rx/[[val]/rx/]//"
+            self.boolean = "/rx/ [x]?OR?/rx/ [ ]"
+            self.description = "/Rx/Description\n/Rx/[description]//"
+            self.footer = "/Rx/[↑ ↓ enter | [exitkey] = /rx/quit/Rx/] EVERYTHING IS /rx/RED/Rx/."
+            self.output = "/Rx/Output//\n/Rx/  [output]//"
+            self.arrows = ["/Rx/▲//", "/Rx/▼//"]
+            self.UPPERONLY = True
